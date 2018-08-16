2f7fe279287dc79439a00841524141fc5d84c2ba
Roman Liubich <roman.liubich@siemens.com>
2018-08-16 09:15:52 +0200

    Add missing libraries for windows shared build

--- conanfile.py
+++ conanfile.py
@@ -48,8 +48,10 @@ class RestccppConan(ConanFile):
             cmake.definitions["RESTC_CPP_WITH_ZLIB"] = "OFF"
         if self.settings.os != "Windows":
             cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
-        if self.settings.compiler == "Visual Studio" and self.settings.arch == "x86" and self.options.shared:
-            cmake.definitions["CMAKE_SHARED_LINKER_FLAGS"] = "/SAFESEH:NO"
+        if self.settings.compiler == "Visual Studio" and self.options.shared:
+            cmake.definitions["CMAKE_SHARED_LINKER_FLAGS"] = "ssleay32 libeay32 crypt32 msi ws2_32"
+            if self.settings.arch == "x86":
+                cmake.definitions["CMAKE_SHARED_LINKER_FLAGS"] = "/SAFESEH:NO" 
         cmake.configure(source_folder="restc-cpp")
         cmake.build()
 
